dca9e0f04146d403a6f3885493bbea26a92d5f9f
Qichen Dong <qidong@cern.ch>
2019-11-26 15:45:49 +0000

    aa

--- code/test.cpp
+++ code/test.cpp
@@ -1,16 +1,18 @@
 #include"genAna.h"
+using namespace std;
 
 int main(){
     TFile * f = TFile::Open("test_out.root");
     TTree* t = (TTree*)f->Get("SM4L_Nominal");
-    std::vector<int> z1_ind;
-    std::vector<int> z2_ind;
+    std::vector<int> *z1_ind;
+    std::vector<int> *z2_ind;
+    t->SetBranchAddress("z1_index", &z1_ind);
+    t->SetBranchAddress("z2_index", &z2_ind);
     auto ne = t->GetEntries();
     for(uint i = 0; i < ne; i++){
-        t->GetEntry();
-        t->SetBranchAddress("z1_index", &z1_ind);
-        t->SetBranchAddress("z2_index", &z2_ind);
+        t->GetEntry(i);
         cout<<i<<":\t";
-        cout<<z1_ind[0]<<"\t"<<z1_ind[1]<<"\t\t"<<z2_ind[0]<<"\t"<<z2_ind[1]<<endl;
+        if(z1_ind->size() > 0)
+            cout<<(*z1_ind)[0]<<"\t"<<(*z1_ind)[1]<<"\t\t"<<(*z2_ind)[0]<<"\t"<<(*z2_ind)[1]<<endl;
     }
 }
